09d810f45fb59a9cc5122e7eafe04dde74b23fd2
Matthew Setter <matthew@maltblue.com>
2016-07-08 10:46:05 +0200

    Use dataProvider annotation for simpler test data creation

--- tests/FizzBuzzTest.php
+++ tests/FizzBuzzTest.php
@@ -14,13 +14,32 @@ class FizzBuzzTest extends \PHPUnit_Framework_TestCase
         $this->fizzBuzz = new FizzBuzz();
     }
 
-    public function testDeterminesFizzBuzzCorrectly()
+    /** @dataProvider listDataProvider */
+    public function testDeterminesFizzBuzzCorrectly($input, $expectedResult)
     {
-        $result = $this->fizzBuzz->process(range(1, 15));
+        $result = $this->fizzBuzz->process($input);
         $this->assertEquals(
             $result,
-            [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz'],
+            $expectedResult,
             'Incorrect result returned: ' . var_export($result, TRUE)
         );
+    }
+
+    public function listDataProvider()
+    {
+        return [
+            [
+                [1, 2, 3, 4, 5],
+                [1, 2, 'Fizz', 4, 'Buzz']
+            ],
+            [
+                [5, 10, 15, 20, 25, 30],
+                ['Buzz', 'Buzz', 'FizzBuzz', 'Buzz', 'Buzz', 'FizzBuzz']
+            ],
+            [
+                [3, 6, 9, 12, 18, 21, 24, 27],
+                ['Fizz', 'Fizz', 'Fizz', 'Fizz', 'Fizz', 'Fizz', 'Fizz', 'Fizz']
+            ],
+        ];
     }
 }
