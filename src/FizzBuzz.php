98794792377fa830bd7782b2465472ab6a9909d0
Matthew Setter <matthew@maltblue.com>
2016-07-08 10:52:24 +0200

    Replace numbers with class constants
    There's no real need to do this. But I wanted to because I like
    readability and don't like magic numbers.

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -9,23 +9,29 @@ namespace FizzBuzz;
  */
 class FizzBuzz
 {
+    const MATCH_FIZZ = 3;
+    const MATCH_BUZZ = 5;
+    const MATCH_FIZZBUZZ = 15;
+
     /**
      * Runs the FizzBuzz algorithm
+     *
      * @param array $list
      */
     public function process(array $list)
     {
         return array_map(function ($item) {
-            if ($item % (3 * 5) === 0) {
+            if ($item % self::MATCH_FIZZBUZZ === 0) {
                 return 'FizzBuzz';
             } else {
-                if ($item % 3 === 0) {
+                if ($item % self::MATCH_FIZZ === 0) {
                     return 'Fizz';
                 }
-                if ($item % 5 === 0) {
+                if ($item % self::MATCH_BUZZ === 0) {
                     return 'Buzz';
                 }
             }
+
             return $item;
         }, $list);
     }
